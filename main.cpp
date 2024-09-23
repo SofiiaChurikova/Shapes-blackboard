d34a6f4338c12e2bbb4b3f86a7056ec9f925a7d0
SofiiaChurikova <sonyachurikova@gmail.com>
2024-09-23 15:14:07 +0300

    Add method to draw Triangles
    add method from file that draws triangles

--- main.cpp
+++ main.cpp
@@ -21,6 +21,37 @@ struct Board {
         }
     }
 };
+class Triangle {
+private:
+    int x, y, height;
+public:
+    Triangle(int x, int y, int height) : x(x), y(y), height(height) {}
+    void drawTriangle(vector<vector<char>>& grid) {
+        if (height <= 0) return;;
+        for (int i = 0; i < height; ++i) {
+            int leftMost = x - i;
+            int rightMost = x + i;
+            int posY = y + i;
+            if (posY < BOARD_HEIGHT) {
+                if (leftMost >= 0 && leftMost < BOARD_WIDTH) {
+                    grid[posY][leftMost] = '*';
+                    if (rightMost >= 0 && rightMost < BOARD_WIDTH && leftMost != rightMost) {
+                        grid[posY][rightMost] = '*';
+                    }
+                }
+            }
+
+        }
+        for (int j = 0; j < 2 * height - 1; ++j) {
+            int baseX = x - height + 1 + j;
+            int baseY = y + height - 1;
+            if (baseX >= 0 && baseX < BOARD_WIDTH && baseY < BOARD_HEIGHT) {
+                grid[baseY][baseX] = '*';
+            }
+        }
+
+    }
+};
 
 class CommandsExecution {
 private:
@@ -44,7 +75,8 @@ public:
             } else if (command == "shapes") {
                 continue;
             } else if (command == "add") {
-                continue;
+                Triangle triangle(10, 1, 5);
+                triangle.drawTriangle(board.grid);
             } else if (command == "undo") {
                 continue;
             } else if (command == "clear") {
